ecc5b2578a94f3b754471d9a43b54850a446095d
Stefan Heinemann <sh@adfinis.com>
2011-10-19 11:23:46 +0200

    Wrote the section on anonymous functions

---
+++ scala/src/anonymousTimer.scala
@@ -0,0 +1,16 @@
+
+object TimerAnonymous {
+	def oncePerSecond(callback: () => Unit) {
+		// Nimm eine Funktion als Argument
+		while (true) {
+			callback() // Rufe die Funktion hier auf
+			Thread sleep 1000
+		}
+	}
+	def main(args: Array[String]) {
+		// Uebergebe oncePerSecond eine anynome Funktion
+		oncePerSecond(() => 
+			println("time flies like an arrow...")
+		)
+	}
+}
