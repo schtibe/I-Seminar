71851eaf4da7d2cbf9a4fb263f3e803f250c236d
Stefan Heinemann <sh@adfinis.com>
2011-10-29 16:40:09 +0200

    Helloworld, var/val, ausführen und kompilieren

--- scala/src/Constructing.scala
+++ scala/src/Constructing.scala
@@ -1,6 +1,5 @@
 
-class Constructing(user: String) {
-	def name = user
+class Constructing(val name: String) {
 	println("Greetings from the constructor to " + this.name)
 
 	def calculate: String = return "2 + 3 is " + (2 + 3)
